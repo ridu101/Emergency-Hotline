d2849d039fd9f6b64143c4a48ff9b1da8feb5d95
ridu101 <ridu116540@gmail.com>
2025-09-07 11:30:29 +0600

    heart icon issue solved

--- home.js
+++ home.js
@@ -1,21 +1,25 @@
-// Select all heart icons
-const heartIcons = document.querySelectorAll(".heart-icon");
 
-// Select the heart count in nav
-const heartCount = document.querySelector(".heart-count");
+const heartIcons = document.querySelectorAll('.heart-icon')
+const heartCount = document.querySelector('.heart-count')
 
-// Counter value
 let count = 0;
 
-// Use for...of loop
 for (let icon of heartIcons) {
-    icon.addEventListener('click', function(){
-        // Increase count
-    count++;
-    // Update nav display
-    heartCount.textContent = count;
-    // Change heart color to red when clicked
-    icon.classList.remove("text-gray-500");
-    icon.classList.add("text-red-500");
+    icon.addEventListener('click', function () {
+
+        count++;
+
+        heartCount.textContent = count;
+        // other icons
+        for (const color of heartIcons) {
+            color.classList.remove("text-red-500");
+            color.classList.add("text-gray-500");
+
+        }
+        //clicked
+        icon.classList.remove('text-gray-500')
+        icon.classList.add('text-red-500')
+       
+
     })
 }
